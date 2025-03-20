4ad65d326dfda3f4dfa38c9e7d5d044a05b3bbcc
Sajeesh V <sajeesh.v.tblr@gmai.com>
2025-03-20 12:08:58 +0530

    modification: used npm packages

--- js-flow/js-script/index.js
+++ js-flow/js-script/index.js
@@ -1,9 +1,11 @@
+const moment = require('moment');
 async function main() {
     console.log('Hello, world!');
-    return "Its working!!!!!!!!!";
+    return `[${moment.utc().format("DD-MM-YYYY hh:mm:s A")}] Its working!!!!!!!!!`;
 }
 (async () => {
-   res= await main();
-   return res;
+    res = await main();
+    console.log(res);
+    return res;
 }
 )()
