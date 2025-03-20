2c50df0ed625590815f6743b2f88186841e7c1c5
Sajeesh V <sajeesh.v.tblr@gmai.com>
2025-03-20 11:59:59 +0530

    python file changes sub processrun absolute path

--- js-flow/flow.py
+++ js-flow/flow.py
@@ -1,12 +1,17 @@
 from prefect import flow, task
+import os
 import subprocess
 
 @task
 def run_js_script():
-    result = subprocess.run(["node", "run", "js-script/index.js"], capture_output=True, text=True)
+    result = subprocess.run(["node", os.path.abspath("js-flow/js-script/index.js")], capture_output=True, text=True)
     return result.stdout
 
 @flow
 def js_workflow():
     output = run_js_script()
     print(f"JS output: {output}")
+
+
+if __name__ == "__main__":
+    js_workflow()
