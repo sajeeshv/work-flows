f5b28086469a9d5bf4379fe37081f91ae4af4f24
Sajeesh V <sajeesh.v.tblr@gmai.com>
2025-03-20 13:23:22 +0530

    path issue chec

--- js-flow/flow.py
+++ js-flow/flow.py
@@ -5,6 +5,10 @@ import subprocess
 @task
 def run_js_script():
     print("JS script PATH",os.path.abspath("./js-flow/js-script/index.js"))
+    if os.path.exists(os.path.abspath("./js-flow/js-script/index.js")):
+        print("File or directory exists")
+    else:
+        print("File or directory does not exist")
     result = subprocess.run(["node", "run",os.path.abspath("./js-flow/js-script/index.js")], capture_output=True, text=True)
     return result.stdout
 
